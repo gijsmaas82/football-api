6551b61e0ad8ba87c72b575a88271b589cc1bd2a
gijsmaas82 <gijsmaas82@outlook.com>
2019-09-04 12:11:57 +0200

    post teams working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const teamRouter = require('./team/router')
-
-const app = express()
-
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
-
+const app = express()
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
 app.use(teamRouter)
 
-app.listen(port, () => console.log('Listening on port:', port))
+app.listen(port, () => console.log('Listening on port:', port)) 
--- team/router.js
+++ team/router.js
@@ -10,4 +10,10 @@ router.get('/team', (req, res, next) => {
     .catch(err => next(err))
 })
 
+router.post('/team', (req, res, next) => {
+  Team.create(req.body)
+    .then(team => res.json(team))
+    .catch(err => next(err))
+})
+
 module.exports = router
