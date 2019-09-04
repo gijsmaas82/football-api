832177c55b4c4ba4e27ed75839cc7e883e94c5b7
gijsmaas82 <gijsmaas82@outlook.com>
2019-09-04 15:41:32 +0200

    forced a reset on my remote db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseUrl)
 
-db.sync({ force: true })
+db.sync()
   .then(() => console.log('Schema has been updated'))
   .catch(console.error)
 
