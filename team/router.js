a3daf3f01b57afbd008407de06ae95962395b6bf
gijsmaas82 <gijsmaas82@outlook.com>
2019-09-04 12:00:02 +0200

    router Class implemented

---
+++ team/router.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+
+const Team = require('./model')
+
+const router = new Router()
+
+router.get('/team', (req, res, next) => {
+  Team.findAll()
+    .then(teams => res.json(teams))
+    .catch(err => next(err))
+})
+
+module.exports = router
